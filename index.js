6258be18f1385fe1a0622616419af8fcfd0ddcc3
Pete <pgchamberlin@googlemail.com>
2016-09-17 14:24:59 +0100

    WIP image data handling (not working yet...)

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var Promise = require('bluebird');
+var getPixels = require('get-pixels');
+var multer = require('multer');
+
+var Swatchmaker = require('./lib/shared/swatchmaker');
 
 var fs = Promise.promisifyAll(require('fs'));
 var engine = require('./lib/mu-express-engine');
@@ -10,26 +14,39 @@ app.set('view engine', 'mu');
 app.set('views', __dirname + '/templates');
 
 var files = {
-    inline_js: "/dist/swatchmaker.js",
+    swatchmaker_js: "/dist/swatchmaker.js",
+    client_js: "/dist/client.js",
     inline_css: "/dist/swatchmaker.css"
 }
 
-app.get('/', function(req, res) {
-    Promise.map([ "inline_css", "inline_js" ], function(file) {
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+
+function render(res, palette, image) {
+    Promise.map([ "inline_css", "swatchmaker_js", "client_js" ], function(file) {
         return fs.readFileAsync(__dirname + files[file]);
     }).then(function(data) {
         res.render(
             'swatchmaker.mu',
             {
                 inline_css: data[0],
-                inline_js: data[1]
+                inline_js: data[1] + data[2],
+                palette: palette,
+                source_image: image
             }
         );
     });
+}
+
+app.get('/', function(req, res) {
+    render(res);
 });
 
-app.post('/', function (req, res) {
-
+app.post('/', upload.single('image'), function (req, res) {
+    getPixels(req.file.buffer, req.file.mimetype, function(err, pixels) {
+        var d = pixels.data;
+        console.log(d[0], d.byteOffset, d.byteLength);
+    });
 });
 
 app.listen(3000, function () {
